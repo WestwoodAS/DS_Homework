a0f1a3008258bffadbbc2522f45cd222b68ae11a
Narwhal-fish <67782688+Narwhal-fish@users.noreply.github.com>
2021-11-23 21:58:39 +0800

    add Ex04 file

---
+++ Ex04/JobQueue.hpp
@@ -0,0 +1,54 @@
+#ifndef _JobQueue_HPP
+#define _JobQueue_HPP
+
+template< typename T >
+class JobQueue {
+	T *cA;
+	int qFront, qBack;
+	int qSize, qMax;
+	
+	int next( int idx ) { return  ((idx + 1 ) % qMax); } // move to next pos
+	int prev( int idx ) { return (( idx + qMax - 1 ) % qMax); } // move to previous pos
+
+public:
+	int avail;
+	JobQueue():cA(NULL), qFront(0), qBack(0), 
+		qSize(0), qMax(0), avail(0) {} // constructor of no-space queue
+		
+	JobQueue<T>( int maxS ):qFront(0), qSize(0), avail(0) {
+		cA = new T [maxS];
+		qMax = maxS;
+		qBack = maxS - 1;
+	} // end constructor
+	
+	int length() const { return qSize; } // return current queue length
+	int isEmpty() const { return (qSize == 0); } // check if queue is empty
+	int isFull() const { return (qSize == qMax); } // check if queue is empty
+	
+	void enQueue( T &newItem ) { // append a new element
+		qSize++; // increase queue length
+		qBack = next(qBack); // locate this last element
+		cA[qBack] = newItem; // call by reference
+	} // end enQueue
+
+	void getFront( T &oldItem ) const { oldItem = cA[qFront]; } // get the first element
+	void deQueue() { // drop the first element
+		qSize--; // decrease queue size
+		qFront = new(qFront); // locate the first element
+	} // end deQueue
+	
+	void deQueue( T &oldItem ) { // get then drop the first element
+		getFront(oldItem);
+		deQueue();
+ 	} // end deQueue
+	
+	void clearQ() { // clean queue
+		while( !isEmpty() ) deQueue();
+		delete [] cA;
+		cA = NULL;
+	} // end clearQ
+	
+	~JobQueue() { clearQ(); } // destructor
+	
+}; // end JobQueue
+#endif //_JobQueue_HPP
---
+++ Ex04/Simulation.hpp
@@ -0,0 +1,25 @@
+#ifndef _Simulation_HPP
+#define _Simulation_HPP
+
+#include "JobList.hpp" // type jobType, class JobList
+#include "JobQueue.hpp" // class JobQueue
+#include "AnsList.hpp" // class AnsList
+
+class Simulation {
+	JobList jobs; // a list of jobs
+	AnsList answer; // a set of answers
+	JobQueue< jobType > **allQ; // a set of queues
+	int qNum; // number of queues
+	
+	void delQ(int); // delete an old job from a queue
+	void updateQ(int,int); // update each queue
+	void finishQ(); // finish the remaining jobs in queue
+	
+public:
+	Simulation( JobList aList, int N ):jobs(aList),qNum(N) // copy constructor
+
+	~Simulation() // destructor
+	void SQF(); // shortest queue first
+
+}; // end Simulation
+#endif // _Simulation_HPP
