6a426434a290859ddd4ea3204cd098322e0a013e
Narwhal-fish <67782688+Narwhal-fish@users.noreply.github.com>
2021-11-24 15:44:25 +0800

    Update JobList.hpp

--- Ex04/JobList.hpp
+++ Ex04/JobList.hpp
@@ -20,21 +20,18 @@ class JobList {
 	vector<jobType> aList;
 	string fileID;
 	
-	void reset() {
-	
-	
-	} // end reset
+	void reset() {aList.clear();} // end reset
 	void showJob(jobType &);
 	void sortByArrival();
 	void putAll();
 	void showTime();
-	void nextJobCheck(jobType &); // get the next job without removal
+	void nextJobCheck(jobType &nextJob) { nextJob = aList.front(); } // get the next job without removal
 	
 public:
 	JobList(){ reset(); }
 	~JobList() { reset(); }
 	
-	bool isEmpty(); // check if it is empty
+	bool isEmpty() { return aList == NULL; } // check if it is empty
 
 	bool getAll(string); // read all from a file;
 	bool getSorted(); // read all from a file and sort them
